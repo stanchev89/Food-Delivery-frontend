a32748defe1734bbd29bd0bb973f07212619408c
Stefan Stanchev <stanchev89@gmail.com>
2021-04-10 22:25:39 +0300

    add meatless switch button

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,24 +3,38 @@ import DishItem from "./DishItem/DishItem";
 import Cart from "../Cart/Cart";
 import foodService from "../../services/foodService";
 import Login from "../Login/Login";
+import {useState} from 'react'
 
 function Menu(props) {
-    const {menu, user, setUser, match, history,setNotification} = props;
+    const {user,setUser,menu, match, history,setNotification} = props;
+    const [meatlessMode, setMeatlessMode] = useState(false);
+
     const addToCart = (dish) => {
         foodService.addToCart(user, dish)
             .then(user => {
                 setUser(user);
             })
             .catch(console.error)
-    }
+    };
+    const toggleMenuToShow = (e) => {
+        setMeatlessMode(prev => !prev);
+    };
+
 
     return (
         <section className="menu">
             <article className="food-list">
+                <article className="toggle-meatless">
+                        <label className="switch">
+                            <input type="checkbox" name='showMeatless' onChange={toggleMenuToShow}/>
+                            <span className="slider round"/>
+                        </label>
+                        <p>безмесни ястия</p>
+                </article>
                 <article className="salads">
                     <h2 className="food-list-title">Салати</h2>
                     <ul>
-                        {menu?.filter((d) => d.category === "salad").map((d) => (
+                        {menu?.filter((d) => d.category === "salad" && (meatlessMode ? d.meatless : true)).map((d) => (
                             <li key={d._id}>
                                 <DishItem dish={d} addToCart={addToCart} isLogged={!!user}/>
                             </li>
@@ -30,7 +44,7 @@ function Menu(props) {
                 <article className="soups">
                     <h2 className="food-list-title">Супи</h2>
                     <ul>
-                        {menu?.filter((d) => d.category === "soup").map((d) => (
+                        {menu?.filter((d) => d.category === "soup" && (meatlessMode ? d.meatless : true)).map((d) => (
                             <li key={d._id}>
                                 <DishItem dish={d} addToCart={addToCart} isLogged={!!user}/>
                             </li>
@@ -40,7 +54,7 @@ function Menu(props) {
                 <article className="main-dishes">
                     <h2 className="food-list-title">Основни ястия</h2>
                     <ul>
-                        {menu?.filter((d) => d.category === "main").map((d) => (
+                        {menu?.filter((d) => d.category === "main" && (meatlessMode ? d.meatless : true)).map((d) => (
                             <li key={d._id}>
                                 <DishItem dish={d} addToCart={addToCart} isLogged={!!user}/>
                             </li>
@@ -50,7 +64,7 @@ function Menu(props) {
                 <article className="deserts">
                     <h2 className="food-list-title">Десерти</h2>
                     <ul>
-                        {menu?.filter((d) => d.category === "desert").map((d) => (
+                        {menu?.filter((d) => d.category === "desert" && (meatlessMode ? d.meatless : true)).map((d) => (
                             <li key={d._id}>
                                 <DishItem dish={d} addToCart={addToCart} isLogged={!!user}/>
                             </li>
@@ -60,7 +74,7 @@ function Menu(props) {
                 <article className="drinks">
                     <h2 className="food-list-title">Напитки</h2>
                     <ul>
-                        {menu?.filter((d) => d.category === "drink").map((d) => (
+                        {menu?.filter((d) => d.category === "drink" && (meatlessMode ? d.meatless : true)).map((d) => (
                             <li key={d._id}>
                                 <DishItem dish={d} addToCart={addToCart} isLogged={!!user}/>
                             </li>
@@ -70,8 +84,8 @@ function Menu(props) {
             </article>
             {
                 user
-                    ? <Cart user={user} setUser={setUser} match={match}/>
-                    : <Login setUser={setUser} history={history} setNotification={setNotification}/>
+                    ? <Cart match={match}/>
+                    : <Login history={history} setNotification={setNotification}/>
             }
         </section>
     );
